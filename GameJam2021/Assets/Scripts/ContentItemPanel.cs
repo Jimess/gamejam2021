ef04fba6bfe110a8d6ee93a61c94f5880b1e8d56
Laurynas <laurynas.pupsta@gmail.com>
2021-01-30 21:32:14 +0200

    super pertvarka

--- GameJam2021/Assets/Scripts/ContentItemPanel.cs
+++ GameJam2021/Assets/Scripts/ContentItemPanel.cs
@@ -6,15 +6,11 @@ public class ContentItemPanel : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI nameMesh;
     [SerializeField] Image spriteImage;
-    [SerializeField] TextMeshProUGUI tierMesh;
-    [SerializeField] Image spriteColor;
 
     public void updateByItem(ContentItem item)
     {
         nameMesh.text = item.itemName;
         spriteImage.sprite = item.sprite;
-        //tierMesh.text = item.itemTier.ToString();
-        //spriteColor.color = item.color;
     }
 
 }
