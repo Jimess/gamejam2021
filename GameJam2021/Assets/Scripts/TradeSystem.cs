441154d14541298e0d635bd0e2ff8a8c9b6d5630
Lokiena <32090082+Lokiena@users.noreply.github.com>
2021-01-30 18:38:41 +0200

    timer
    timer

--- GameJam2021/Assets/Scripts/TradeSystem.cs
+++ GameJam2021/Assets/Scripts/TradeSystem.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TradeSystem : MonoBehaviour
 {
     public static TradeSystem Instance;
-    private ContentItem currentPlayerItem;
+    
     [SerializeField] PlayerItem playerItem;
     [SerializeField] Trader trader1;
     [SerializeField] Trader trader2;
+    [SerializeField] Slider timeCounter;
+    [SerializeField] float timeLimit = 5.0f;
+
+    private ContentItem currentPlayerItem;
+    private float startTime;
+    
 
     private void Awake()
     {
@@ -20,12 +27,43 @@ public class TradeSystem : MonoBehaviour
     {
         ContentItem testItem1 = GameContent.Instance.content[0];
         updateTrader(trader1, testItem1);
+        StartCoroutine(TradeStart());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            TogglePause();
+        }
+        timeCounter.value = Time.time - startTime;
+    }
+
+    void TogglePause()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            Debug.Log("PAUSED");
+        }
+        else
+        {
+            Time.timeScale = 1;
+            Debug.Log("UNPAUSED");
+        }
+    }
+
+    IEnumerator TradeStart()
+    {
+        startTime = Time.time;
+        timeCounter.maxValue = timeLimit;
+        while(Time.time - startTime < timeLimit)
+        {
+            yield return null;
+        }
+        Debug.Log("Trade ended");
+        //cia viskas po trade end
     }
 
     public void SelectItem(ContentItem selectedItem)
